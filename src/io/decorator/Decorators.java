5d817b5346d46748feb7901c9829213fb08bbf34
hongzhi.xu <hongzhi.xu@shuyun.com>
2019-12-20 09:48:49 +0800

    整理io

---
+++ src/io/decorator/Decorators.java
@@ -0,0 +1,20 @@
+package io.decorators;
+
+import java.io.*;
+
+/**
+ * 装饰类
+ */
+public class Decorators extends FilterInputStream {
+    protected Decorators(InputStream in) {
+        super(in);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file=new File("d:/a.txt");
+        FileInputStream fileInputStream=new FileInputStream(file);
+        Decorators decorators=new Decorators(fileInputStream);
+
+
+    }
+}
