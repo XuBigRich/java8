6da1f8d55f5e8be046a2e2c59edb4d2ffdf33ec6
mac <847118663@qq.com>
2021-07-26 07:55:31 +0800

    转unicode

--- src/baozhuanglei/BaozhuangDemo.java
+++ src/baozhuanglei/BaozhuangDemo.java
@@ -3,18 +3,17 @@ package baozhuanglei;
 /**
  * 那么包装类型都有那些
  * 包装类型中0是否可以当除数？ 当除数返回的是什么？
- *
- *
- *       基本类型 与 包装类
- *      boolean —> Boolean
- *       char —> Character
- *       byte—> Byte
- *       short—> Short
- *       long—> Long
- *       int —> Integer
- *       float—> Float
- *       double—> Double
-
+ * <p>
+ * <p>
+ * 基本类型 与 包装类
+ * boolean —> Boolean
+ * char —> Character
+ * byte—> Byte
+ * short—> Short
+ * long—> Long
+ * int —> Integer
+ * float—> Float
+ * double—> Double
  *
  * @author admin
  * @version 1.0.0
@@ -24,42 +23,84 @@ package baozhuanglei;
  */
 public class BaozhuangDemo {
     //包装类的使用
-    public static void use(){
+    public static void use() {
         //以 int 与 Integer 为例   其他 包装类 原理 相似
-        int a=new Integer(2);
-        int b=2;
-        Integer c=b;
+        int a = new Integer(2);
+        int b = 2;
+        Integer c = b;
         System.out.println(c);
     }
-    //包装类型的特色 0可以除以0 不会抛出异常，返回值用NaN标表示这个数字不存在
-    public static void zeroDividedZero(){
+
+    //char
+    public static void learChar() {
         //Character
-        Character character='a';
-        //Double
-        Double doubleZero1=0.0;
-        Double doubleZero2=0.0;
-        Double b=doubleZero1/doubleZero2;
+        Character character = 'a';
+        Character character1 = 49;
+        Character character2 = '\u7b80';
+        System.out.printf("%s,%s,%s", character, character1, character2);
+        System.out.println();
+    }
+
+    //float的学习  测试是否可以除以0  可以0.0/0.0
+    public static void learFloat() {
+        Double b = 0.0 / 0.0;
         //Float
-        Float floatZero1=0.0f;
-        Float floatZero2=0.0f;
-        Float c=floatZero1/floatZero2;
-        System.out.println(b+" "+c);
-        System.err.println(b.equals(b));
+        Float floatZero1 = 0.0f;
+        Float floatZero2 = 0.0f;
+        Float c = floatZero1 / floatZero2;
+        System.out.println(b + " " + c);
         System.err.println(b.equals(c));
-        System.out.println(b==b);
-        //Integer  只有浮点类型可以进行0/0 返回NuN （不存在的数字） 整形是不可以进行 0/0的
-        Integer intZero1=0;
-        Integer intZero2=0;
-        Integer a=intZero1/intZero2;
-        System.out.println(a);
+    }
 
-
+    //double的学习  测试是否可以除以0  可以0.0/0.0
+    public static void learDouble() {
+        //Double
+        Double doubleZero1 = 0.0;
+        Double doubleZero2 = 0.0;
+        Double b = doubleZero1 / doubleZero2;
+        System.err.println(b.equals(b));
+        System.out.println(b == b);
     }
 
 
+    //Integer  只有浮点类型可以进行0/0 返回NuN （不存在的数字） 整形是不可以进行 0/0的
+    public static void learInteger() {
+        //Integer  只有浮点类型可以进行0/0 返回NuN （不存在的数字） 整形是不可以进行 0/0的
+        Integer intZero1 = 0;
+        Integer intZero2 = 0;
+        Integer a = intZero1 / intZero2;
+        System.out.println(a);
+    }
+
+    /**
+     * 扩展 中文转unicode编码
+     */
+    public static String gbEncoding(final String gbString) {
+        char[] utfBytes = gbString.toCharArray();
+        String unicodeBytes = "";
+        for (int i = 0; i < utfBytes.length; i++) {
+            //将字符串中的每一个字符取出， 调用Integer toHexString，这个是专门映射字符到16进制的 方法
+            String hexB = Integer.toHexString(utfBytes[i]);
+            //如果16进制长度小于一个字节 （byte），那就补足为两个字节
+            if (hexB.length() <= 2) {
+                hexB = "00" + hexB;
+            }
+            //  添加/u表示 标识这是一个unicode
+            unicodeBytes = unicodeBytes + "\\u" + hexB;
+        }
+        return unicodeBytes;
+    }
+
     public static void main(String[] args) {
-        use();
-        zeroDividedZero();
+//        use();
+//        learChar();
+//        learFloat();
+//        learDouble();
+//        learInteger();
+//        System.out.println(Integer.toHexString(' '));
+        char a='\u0000';
+        System.out.println("====");
+        System.out.println(a);
     }
 
 }
