0fa67c23bca80a1d698cfa07d44b30b6c6157eb6
许鸿志 <847118663@qq.com>
2021-07-26 15:40:50 +0800

    中文

--- src/charDemo/CharDemo.java
+++ src/charDemo/CharDemo.java
@@ -8,13 +8,26 @@ package charDemo;
  * @createTime 2019年04月18日 20:08:00
  */
 public class CharDemo {
-    char a='a'; // char的 赋值 只能用  单引号
+    char a = 'a'; // char的 赋值 只能用  单引号
     //char b=' a' //空格也占用字节
-    char c=' '; //同样 char也可以储存一个 空格
+    char c = ' '; //同样 char也可以储存一个 空格
     //char d='ad';  //char只能储存一个 字符 这样是2个多了
 //    char e='许'; //char 可以储存一个 汉字
 
+    public static void use() {
+        //unicode 码中的 十进制表示
+        char Int = 35768;
+        System.out.println(Int);
+        //unicode 码中的 16进制表示
+        char unicode = '\u8bb8';
+        System.out.println(unicode);
+        //直接上中文
+        char chinese = '许';
+        System.out.println(chinese);
+    }
+
     public static void main(String[] args) {
-        CharDemo charDemo=new CharDemo();
+        CharDemo charDemo = new CharDemo();
+        use();
     }
 }
