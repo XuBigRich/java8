b64eba2c71634ea742997fc8c79b48d4cd936e96
许鸿志 <847118663@qq.com>
2023-04-04 17:06:21 +0800

    剪切list 判断其原理

--- src/rongqi/ListDemo/SubListDemo.java
+++ src/rongqi/ListDemo/SubListDemo.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 /**
  * 验证  SubList是深复制 还是浅复制， 他俩的顺序还不能反 ，否则会出现 异常
+ *
  * @Author： hongzhi.xu
  * @Date: 2023/4/4 4:45 下午
  * @Version 1.0
  */
 public class SubListDemo {
-    public static void main(String[] args) {
-        List<Integer> integers=new ArrayList<>();
+    public static void read() {
+        List<Integer> integers = new ArrayList<>();
         integers.add(1);
         integers.add(2);
         integers.add(3);
@@ -19,11 +20,31 @@ public class SubListDemo {
         integers.add(5);
         integers.add(6);
 
-        List sub=integers.subList(0,3);
+        List sub = integers.subList(0, 3);
         //Exception in thread "main" java.util.ConcurrentModificationException
 //        integers.add(1,666);
-        sub.add(1,666);
+        sub.add(1, 666);
         System.out.println(integers);
         System.out.println(sub);
+    }
+
+    public static void isnull() {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+
+        List sub = integers.subList(0, 3);
+        integers = null;
+        System.out.println(integers);
+        System.out.println(sub);
+    }
+
+    public static void main(String[] args) {
+//        read();
+        isnull();
     }
 }
