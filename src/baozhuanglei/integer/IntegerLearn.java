9a5e783f308a69a6d9896a907cb0ca1839ecd850
许鸿志 <847118663@qq.com>
2025-06-04 08:20:19 +0800

    一个Integer拆包小例子

---
+++ src/baozhuanglei/integer/IntegerLearn.java
@@ -0,0 +1,16 @@
+package baozhuanglei.integer;
+
+/**
+ * @Author： hongzhi.xu
+ * @Date: 2025/6/4 08:13
+ * @Version 1.0
+ */
+public class IntegerLearn {
+    //一个Integer拆包小例子
+    public static void main(String[] args) {
+        Integer a = null;
+        if (a > 1) {
+            System.out.println("成功执行");
+        }
+    }
+}
